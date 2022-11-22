f44b013667b7740131e73f35acfbc02d8ebe4ec9
yoonseo1 <81469331+yoonseo1@users.noreply.github.com>
2022-11-22 11:34:00 +0900

    add Search Page

--- main.js
+++ main.js
@@ -60,31 +60,32 @@ var app = http.createServer(function (request, response) {
           i = i + 1;
         }
         list = list + "</ul>"; //파일 이름 받아서 반복문으로 목록을 출력
-
-        var title = queryData.id;
-        var template = `
-      <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">Bookea</a></h1>
-      <ul>
-        <li><a href="/?id=Join">회원가입</a></li>
-        <li><a href="/?id=Login">로그인</a></li>
-        <li><a href="/?id=MainPage">메인화면</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    
-      `;
-        console.log(description);
-        response.writeHead(200);
-        response.end(template);
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var title = queryData.id;
+            var template = `
+        <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">Bookea</a></h1>
+        ${list}
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      
+        `;
+            console.log(description);
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
       });
     }
   } else {
