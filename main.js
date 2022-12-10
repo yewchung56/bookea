8ff0c490806873163308435009267d7c2a2ad714
GyuriShim <rabitskr@naver.com>
2022-12-10 21:45:34 +0900

    .

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
-var db = require("./lib/db");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
