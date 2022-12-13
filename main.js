a5ee3be629c2a3e27c9d1f94aaafc46cc69f82c4
yoonseo1 <81469331+yoonseo1@users.noreply.github.com>
2022-12-13 15:13:10 +0900

    mylibrary swal 날짜, 리뷰, 인용구 반복 띄우기 하는중

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
+const glob = 100;
+module.exports = glob;
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
